53f7cbb3f061b5c07ae0ee15f5c1d4a66d3abf1a
Samuel Velasquez <samuelvelasquez@protonmail.com>
2019-10-26 14:08:38 -0400

    WIP: reddit clone tutorial implementation

---
+++ mhh-woeboard/src/containers/App/index.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import './App.css';
+import * as firebase from "firebase";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        Hello World
+      </div>
+    );
+  }
+}
+
+export default App;
