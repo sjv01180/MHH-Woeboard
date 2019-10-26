1a83e1b4a8980c3a9e22e5fea27a52a060a621ef
Aster Patscott <eep5813@gmail.com>
2019-10-26 16:34:42 -0400

    did some html stuff but it broken :(

--- mhh-woeboard/src/containers/App/index.js
+++ mhh-woeboard/src/containers/App/index.js
@@ -1,11 +1,17 @@
 import React, {Component} from 'react';
 import './App.css';
-import * as firebase from "firebase";
+
 
 class App extends Component {
   render() {
     return (
+ 
+
       <div className="App">
+        <nav>
+       
+        </nav>
+
         Hello World
       </div>
     );
--- mhh-woeboard/src/routes.js
+++ mhh-woeboard/src/routes.js
@@ -2,16 +2,13 @@ import React from 'react';
 import {BrowserRouter, Route} from 'react-router-dom';
 
 import App from './containers/App';
-var firebase = require('firebase/app') ;
 
-import "firebase/auth" ;
-import "firebase/firestore" ;
+
 
 const Routes = (props) => (
   <BrowserRouter>
     <Route path="/" component={ App } />
-    <Route path="/hi" component={ <div>henlo wolrd</div> } >
-    </Route>
+    
   </BrowserRouter>
 );
 
