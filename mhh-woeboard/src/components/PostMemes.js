bc987817f185482867e00b96e746b35727edf9aa
Sonya Mac <sonyansachdev@gmail.com>
2019-10-27 12:54:46 -0400

    Added Description and Styled Gallery

--- mhh-woeboard/src/components/PostMemes.js
+++ mhh-woeboard/src/components/PostMemes.js
@@ -41,30 +41,39 @@ class PostMemes extends Component {
         <SiteHeader />
         <div className="panel panel-default">
           <div className="panel-heading">
-            <h3 className="panel-title">
+            <h2 className="panel-title">
               Exquisite Memes
-            </h3>
+            </h2>
             <ImageExporter />
           </div>
           <div className="panel-body">
           <div id="gallery">
           <ul class="nivo">
             <li>
+            <a>
+            <span>cute puppies</span>
             <a href="images/106927941_pa-6355447.jpg"><img src={require("./images/106927941_pa-6355447.jpg")} alt="doggo" width="300" height="200"/>
-            <span>cute puppies</span></a>
-            </li>
-            <li>
-            <a href="images/skynews-kitten-cat_4627790.jpg"><img src={require("./images/skynews-kitten-cat_4627790.jpg")} alt="kittens" width="600" height="400"/>
-            <span>cute kittens</span></a>
-            </li>
-            <li>
-            <a href="images/garfeild.jpg"><img src={require("./images/garfeild.jpg")} width="600" height="400"/>
-            <span>eldrich cat</span></a>
-            </li>
-            <li>
-            <a href="images/garfeildwithoutgarfeild.jpg"><img src={require("./images/garfeildwithoutgarfeild.jpg")} width="600" height="400"/>
-            <span>mid life crisis</span>
             </a>
+            </a>
+            </li>
+            <li>
+            <a>
+            <span>cute kittens</span>
+            <a href="images/skynews-kitten-cat_4627790.jpg"><img src={require("./images/skynews-kitten-cat_4627790.jpg")} alt="kittens" width="600" height="400"/>
+            </a>
+            </a>
+            </li>
+            <li>
+            <a>
+            <span>eldrich cat</span>
+            <a href="images/garfeild.jpg"><img src={require("./images/garfeild.jpg")} width="600" height="400"/>
+            </a></a>
+            </li>
+            <li>
+            <a>
+            <span>mid life crisis</span>
+            <a href="images/garfeildwithoutgarfeild.jpg"><img src={require("./images/garfeildwithoutgarfeild.jpg")} width="600" height="400"/>
+            </a></a>
             </li>
           </ul>
           </div>
